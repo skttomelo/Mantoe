aaf26b2c88a5b98836c81bc4665064c4223a3b59
Trevor <trevor.h.crow@gmail.com>
2018-10-30 13:50:32 -0400

    end of class work

--- Web Programming/In-class/10-30-18/basicphp.php
+++ Web Programming/In-class/10-30-18/basicphp.php
@@ -5,7 +5,18 @@
     </head>
     <body>
         <?php
+            class Car(){
+                function Car(){
+                    $this->model = "VW";
+                }
+            }
             echo "My first PHP script!";
+            echo "<h1>Sup bitch</h1>";
+            print "My last PHP line <br>";
+            $color = "blue";
+            echo "the car is ".$color."<br>";
+            $herbie = new Car();
+            echo $herbie->model;
         ?>
     </body>
 </html>
