e60b80dff83d733855b20c41646a3a2d0a09aef9
Trevor <trevor.h.crow@gmail.com>
2018-11-08 13:36:41 -0500

    REEEEEEEEEEE ITS BROKEN

--- Web Programming/Homeworks/HW 7/guessing_game.js
+++ Web Programming/Homeworks/HW 7/guessing_game.js
@@ -3,18 +3,24 @@ Name(s): Trevor Crow
 Date: 11/8/2018
 Description: Number Guessing Game
 */
-
 var R = 1+Math.floor(Math.random()*100); //assigns a random number between 1 and 100
+var score = 50;
 
 function checkGuess(){
-    var G = document.getElementById("guess");
+    var G = Number(document.getElementById("guess").value);
     if(isNaN(G)){
         document.getElementById("distance").innerHTML = "<i><u>Please</u> enter something that is a number!</i>";
     }else{
-        if(G < 1 || G > 100){
+        if((G < 1) || (G > 100)){
             document.getElementById("distance").innerHTML = "<i><u>Please</u> enter a number that is <u><b>BETWEEN</b></u> 1 and 100!</i>";
         }else{
-
+            if(80<=(G-R)){
+                document.getElementById("distance").innerHTML = "You are very cold";
+                score -= 5;
+            }else if(60<=(G-R)<80){
+                document.getElementById("distance").innerHTML = "You are cold";
+                score -= 5;
+            }
         }
     }
 }
--- Web Programming/In-class/11-6-18/validation.php
+++ Web Programming/In-class/11-6-18/validation.php
@@ -1,5 +1,12 @@
 <?php
     $name=$email=$gender=$comment=$website="";
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $name = test_input($_POST["name"]);
+        $email = test_input($_POST["email"]);
+        $gender = test_input($_POST["gender"]);
+        $comment = test_input($_POST["comment"]);
+        $website = test_input($_POST["website"]);
+    }
     function test_input($data){
         $data = trim($data);
         $data = stripslashes($data);
