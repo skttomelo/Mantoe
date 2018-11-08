088587acc762b77ea574b949214e6a10c02853dd
Trevor <trevor.h.crow@gmail.com>
2018-11-08 04:10:59 -0500

    descriptions added and js work begun
    css is gonna be a drag once I start to work on that

--- Web Programming/Homeworks/HW 7/guessing_game.js
+++ Web Programming/Homeworks/HW 7/guessing_game.js
@@ -1,5 +1,20 @@
 /*
 Name(s): Trevor Crow
 Date: 11/8/2018
-Description: 
+Description: Number Guessing Game
 */
+
+var R = 1+Math.floor(Math.random()*100); //assigns a random number between 1 and 100
+
+function checkGuess(){
+    var G = document.getElementById("guess");
+    if(isNaN(G)){
+        document.getElementById("distance").innerHTML = "<i><u>Please</u> enter something that is a number!</i>";
+    }else{
+        if(G < 1 || G > 100){
+            document.getElementById("distance").innerHTML = "<i><u>Please</u> enter a number that is <u><b>BETWEEN</b></u> 1 and 100!</i>";
+        }else{
+
+        }
+    }
+}
