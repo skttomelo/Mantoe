bdde36317528983efc8fc4ea4e4cd419bd663d88
Trevor <trevor.h.crow@gmail.com>
2018-11-11 18:07:26 -0500

    JS is complete for the homework now

--- Web Programming/Homeworks/HW 7/guessing_game.js
+++ Web Programming/Homeworks/HW 7/guessing_game.js
@@ -3,9 +3,14 @@ Name(s): Trevor Crow
 Date: 11/8/2018
 Description: Number Guessing Game
 */
-var R = 1+Math.floor(Math.random()*100); //assigns a random number between 1 and 100
+
+function randomNum(x,y){//random number generator between x and y
+    return x+Math.floor(Math.random()*y)
+}
+
+var R = randomNum(1,100); //assigns a random number between 1 and 100
 var score = 50;
-console.log(R);
+var win = false;
 
 function checkGuess(){
     var G = Number(document.getElementById("guess").value);
@@ -14,29 +19,39 @@ function checkGuess(){
     }else{
         if((G < 1) || (G > 100)){
             document.getElementById("distance").innerHTML = "<i><u>Please</u> enter a number that is <u><b>BETWEEN</b></u> 1 and 100!</i>";
-        }else{
-            if(80<=(G-R)){
+        }else{ //check how far or how close the player is to the answer
+            if(Math.abs(G-R)>=80){
                 document.getElementById("distance").innerHTML = "You are very cold";
                 score -= 5;
-            }else if(60<=(G-R)<80){
+            }else if(Math.abs(G-R)>=60){
                 document.getElementById("distance").innerHTML = "You are cold";
                 score -= 5;
-            }else if(40<=(G-R)<60){
+            }else if(Math.abs(G-R)>=40){
                 document.getElementById("distance").innerHTML = "You are warm";
                 score -= 5;
-            }else if(20<=(G-R)<40){
+            }else if(Math.abs(G-R)>=20){
                 document.getElementById("distance").innerHTML = "You are hot";
                 score -= 5;
-            }else if(0<(G-R)<20){
+            }else if(Math.abs(G-R)>0){
                 document.getElementById("distance").innerHTML = "You are burning";
                 score -= 5;
-            }else if((G-R)==0){
-                document.getElementById("distance").innerHTML = "You got it!";
-                score -= 5;
+            }else if(Math.abs(G-R)==0){
+                document.getElementById("distance").innerHTML = "You got it! If you would like to play again, please enter a new guess.";
+                win = true;
             }
 
             if(score<=0){
-                
+                document.getElementById("distance").innerHTML = "You lose, the number was actually " + R + "! If you'd like to try again, enter a new guess.";
+                document.getElementById("score").innerHTML = "0";
+                score = 50; //resetting score
+                R = randomNum(1,100); //picking a new number between 1 and 100
+            }else{
+                document.getElementById("score").innerHTML = score;
+                if(win == true){
+                    score = 50; //resetting score
+                    R = randomNum(1,100); //picking a new number between 1 and 100
+                    win = false;
+                }
             }
         }
     }
