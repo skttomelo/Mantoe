b3fab9b964563c4fbe16465b79cc4ac234ebe485
Trevor <trevor.h.crow@gmail.com>
2018-11-15 14:38:48 -0500

    more work on the mysql stuff from the obj oriented side

--- Web Programming/In-class/11-13-18/MySQLi Object-Oriented.php
+++ Web Programming/In-class/11-13-18/MySQLi Object-Oriented.php
@@ -2,20 +2,38 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $dbname = "test";
     //create connection
-    $conn = new mysqli($servername,$username,$password);
+    $conn = new mysqli($servername,$username,$password, $dbname);
     //check connection
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
     echo "Connected successfully";
-    //create database
-    $sql = "CREATE DATABASE myDB";
+    //create table
+    $sql = "CREATE TABLE MyGuest(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstname varchar(30) not null,
+            lastname varchar(30) not null,
+            email varchar(50),
+            reg_date TIMESTAMP
+    )";
     if($conn->query($sql) === true){
-        echo "Database created successfully";
+        echo "Table created successfully";
     }else{
-        echo "Error creating database: ".$conn->error;
+        echo "Error creating Table: ".$conn->error;
+    }
+    $stmt = $conn->prepare("INSERT INTO MyGUests(firstname, lastname, email) VALUES(?, ?, ?)");
+    $firstname = "Celena";
+    $lastname = "Crow";
+    $email = "Celena.E.Crow@gmail.com";
+    $stmt->bind_param("sss", $firstname, $lastname, $email);
+    if($conn->query($sql) === true){
+        $last_id = $conn->insert_id;
+        echo "Record created successfully";
+    }else{
+        echo "Error creating Record: ".$conn->error;
     }
 
+    $stmt->close();
     $conn->close();
 ?>
